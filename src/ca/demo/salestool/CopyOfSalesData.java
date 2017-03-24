7d6c567c3c1c5f78e44f414692b4c3df0e493b6d
Tran Van Phuc <phuctranifi@gmail.com>
2017-03-25 05:55:56 +0700

    Commit new file

---
+++ src/ca/demo/salestool/CopyOfSalesData.java
@@ -0,0 +1,21 @@
+package ca.demo.salestool;
+
+/**
+ * Represent the sales data for a given period.
+ */
+public class CopyOfSalesData {
+	int data[] = {-1, 0, 4, 42, 84};
+	
+	public void display() {
+		System.out.println("Data: ");
+
+		int sum = 0;
+		for (int i: data) {
+			System.out.println("Next value: " + data[i]);
+			sum += i;
+		}
+		
+		System.out.println("Sum is: " + sum);
+	}
+
+}
